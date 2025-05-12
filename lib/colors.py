e6f06c7277767499c37bfadec46c346faa67c7df
younglet <younglet@qq.com>
2025-05-12 16:24:33 +0800

    Add:添加颜色常量模块以及RGB灯带测试程序

---
+++ lib/colors.py
@@ -0,0 +1,59 @@
+# 定义颜色常量
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+RED = 255, 0, 0
+ORANGE = 255, 165, 0
+YELLOW = 255, 255, 0
+GREEN = 0, 255, 0
+CYAN = 0, 255, 255  # 青色
+BLUE = 0, 0, 255
+PURPLE = 128, 0, 128
+MAGENTA = 255, 0, 255  # 洋红色
+
+MAROON = 128, 0, 0  # 褐红色
+OLIVE = 128, 128, 0  # 橄榄绿
+LIME = 0, 255, 0  # 和绿色相同，但通常表示更亮的绿色
+TEAL = 0, 128, 128  # 蓝绿色
+NAVY = 0, 0, 128  # 海军蓝
+
+# 包含所有颜色变量的列表
+COLORS = [
+    WHITE,
+    BLACK,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    CYAN,
+    BLUE,
+    PURPLE,
+    MAGENTA,
+    MAROON,
+    OLIVE,
+    LIME,
+    TEAL,
+    NAVY
+]
+
+# 包含彩虹颜色变量的列表
+RAINBOW = [
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    CYAN,
+    BLUE,
+    PURPLE
+]
+
+# 冷色到暖色的渐变列表
+VOLUME_BAR = [
+    BLUE,    # 冷色调开始
+    CYAN,
+    TEAL,
+    GREEN,
+    LIME,
+    YELLOW,
+    ORANGE, 
+    RED  # 暖色调结束
+]
