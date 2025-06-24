76fae6ba79d42018338021dcb91d30ccee4f9426
younglet <younglet@qq.com>
2025-06-24 09:37:19 +0800

    Add:添加espNOW通信实现基础项目

---
+++ demo/esp_now_basic_communication/receiver.py
@@ -0,0 +1,26 @@
+import network
+import espnow
+import time
+
+from machine import Pin
+from led import LED
+
+led = LED(Pin(2))
+led.blink()
+
+sta = network.WLAN(network.WLAN.IF_STA) 
+sta.active(True)
+print("我的MAC地址是：", sta.config('mac'))
+
+e = espnow.ESPNow()
+e.active(True)
+peer_mac = b'\xd0\xefv\xee\xfb\xe8'
+e.add_peer(peer_mac)
+
+
+while True:
+    if e.any():
+        peer_mac, msg = e.recv()
+        led.blink(3, 0.1)
+        print("收到消息：", msg)
+    time.sleep(0.2)
---
+++ demo/esp_now_basic_communication/sender.py
@@ -0,0 +1,24 @@
+import network
+import espnow
+import time
+
+from machine import Pin
+from led import LED
+
+led = LED(Pin(2))
+led.blink()
+
+sta = network.WLAN(network.WLAN.IF_STA)
+sta.active(True)
+print("我的MAC地址是：", sta.config('mac'))
+
+e = espnow.ESPNow()
+e.active(True)
+peer_mac = b'\xd0\xefv\xee\xfb\xe8'
+e.add_peer(peer_mac)
+
+
+while True:
+    e.send(peer_mac, "Hello from B")
+    led.blink(3, 0.1)
+    time.sleep(2)
