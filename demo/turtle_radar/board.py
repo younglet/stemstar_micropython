137f8754ae20742e263bfa91611d5f12343714b6
younglet <younglet@qq.com>
2025-10-16 15:26:43 +0800

    Optimize:优化代码和注释

--- demo/turtle_radar/board.py
+++ demo/turtle_radar/board.py
@@ -1,31 +1,29 @@
 from sg90 import SG90
-from machine import UART, Pin
+from machine import Pin, UART
 from hcsr04 import HCSR04
-import time
+from time import sleep_ms
 
-# 初始化串口通信
-uart = UART(2, baudrate=115200, tx=Pin(26), rx=Pin(27))
-# 初始化距离传感器
-sensor = HCSR04(Pin(32), Pin(33))
-# 初始化舵机
-servo = SG90(Pin(4))
 
-# 变量初始化
-angle = 10 # 初始角度
-step = 5   # 每次移动的角度步长
+uart = UART(2, baudrate=115200)                      # 初始化串口通信
+sensor = HCSR04(Pin(32), Pin(33))                    # 初始化距离传感器
+servo = SG90(Pin(4))                                 # 初始化舵机
+
+angle = 10                                           # 初始角度变量
+step = 5                                             # 每次移动的角度步长
 
 while True:
-    # 控制舵机移动到指定角度，并测量距离
-    servo.move_to(angle)
-    distance = sensor.get_distance()
-
-    #整理角度和距离数据，并通过串口发送
-    msg = f"角度: {angle} 距离: {distance:.1f} \n"
-    uart.write(msg)
-    print(msg)
     
-    # 更新角度，控制舵机在10到170度之间来回摆动
-    angle += step
-    if angle >= 170:
-        angle = 10
-    time.sleep(0.5)
+    angle += step                                    # 更新角度
+    servo.move_to(angle)                             # 控制舵机移动到指定角度
+    distance = sensor.get_distance()                 # 获取距离数据
+
+    msg = f"角度: {angle} 距离: {distance:.1f} \n"    # 格式化字符串
+    uart.write(msg)                                  # 通过串口发送数据
+    print(msg)                                       # 在控制台打印发送的数据
+    
+    if angle >= 170:                                 # 超出右边界
+        step = -5                                    # 开始往回扫
+    elif angle <= 10:                                # 超出左边界
+        step = 5                                     # 开始向前扫
+
+    sleep_ms(50)                                     # 延时50毫秒，等待舵机稳定
--- demo/turtle_radar/pc.py
+++ demo/turtle_radar/pc.py
@@ -15,36 +15,36 @@ def emit_line(angle, distance, scale=10):
     :param angle: 线段的角度
     :param distance: 线段的距离
     """
-    if angle == 10:                 #  如果当前角度为10度，则清空画布
+    if angle == 10:                                    #  如果当前角度为10度，则清空画布
         turtle.clear()    
     
-    turtle.penup()                  # 画笔抬起
-    turtle.goto(0, -300)            # 画笔移动到画布底部中心
-    turtle.setheading(angle)        # 画笔朝向当前角度
+    turtle.penup()                                     # 画笔抬起
+    turtle.goto(0, -300)                               # 画笔移动到画布底部中心
+    turtle.setheading(angle)                           # 画笔朝向当前角度
 
-    turtle.pendown()                # 画笔落下
-    turtle.pencolor('green')        # 画笔颜色为蓝色
-    turtle.forward(distance*scale)  # 画笔向前移动，线段长度为距离*缩放比例
-    turtle.pencolor('red')          # 画笔颜色为红色
-    turtle.forward(1000)            # 确保线段能铺满画布
+    turtle.pendown()                                   # 画笔落下
+    turtle.pencolor('green')                           # 画笔颜色为蓝色
+    turtle.forward(distance*scale)                     # 画笔向前移动，线段长度为距离*缩放比例
+    turtle.pencolor('red')                             # 画笔颜色为红色
+    turtle.forward(1000)                               # 确保线段能铺满画布
 
 
 # 配置串口参数
-SERIAL_PORT = '/dev/cu.wchusbserial14220'      # 根据实际情况修改为你的串口号   
-BAUD_RATE = 115200                             # 波特率，固定为115200
-TIMEOUT = 1                                    # 读超时设置，单位秒
+SERIAL_PORT = '/dev/cu.wchusbserial14220'              # 根据实际情况修改为你的串口号   
+BAUD_RATE = 115200                                     # 波特率，固定为115200
+TIMEOUT = 1                                            # 读超时设置，单位秒
 
 # 打开串口，如果打不开此步会运行报错，请检查串口参数
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
 print(f"已连接到 {SERIAL_PORT} @ {BAUD_RATE} baud")
 
 while True:
-    if ser.in_waiting > 0:                    # 如果有数据可读
-        data = ser.readline().decode('utf-8') # 读取一行数据（以 \n 结尾）
-        print(data)                           # 输出接受的数据
-        data = data.split(' ')                # 利用空格分割数据
-        angle = float(data[1])                # 获取当前角度
-        distance = float(data[3])             # 获取当前距离
-        emit_line(angle, distance)            # 画线
+    if ser.in_waiting > 0:                            # 如果有数据可读
+        res = ser.readline().decode('utf-8').strip()  # 读取一行数据（以 \n 结尾）
+        data = res.split(' ')                         # 利用空格分割数据
+        print(data)                                   # 输出接收的数据
+        angle = float(data[1])                        # 获取当前角度
+        distance = float(data[3])                     # 获取当前距离
+        emit_line(angle, distance)                    # 画线
     else:
-        time.sleep(0.1)                       # 暂停0.1秒后继续循环
+        time.sleep(0.1)                               # 暂停0.1秒后继续循环
